3a2ec0013f035270e5d3d7390d8820e97e809eb2
Leonid Fishkis <leonidfishkis@gmail.com>
2021-06-16 09:57:56 +0200

    users list from database

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import User from './User';
+import { useQuery, gql } from '@apollo/client';
+
+const USERS_QUERY = gql`
+  query {
+    users {
+      name
+      id
+    }
+  }
+`;
 
 const UsersList = () => {
-  const usersToRender = [
-    {
-      id: '1',
-      name:
-        'xxx',
-    },
-    {
-      id: '2',
-      name: 'yyy',
-    }
-  ];
+  const { data, error, loading } = useQuery(USERS_QUERY);
+  //console.log(loading)
+  //console.log(error)
+  //console.log(data)
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
   return (
     <div>
-      {usersToRender.map((user) => (
-        <User key={user.id} user={user} />
-      ))}
+      {data && (
+        <>
+          {data.users.map((user) => (
+            <User key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
