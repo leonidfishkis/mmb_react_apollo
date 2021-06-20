dca5bf18b6df1c5e33010b49c3136a2ae8faff25
Leonid Fishkis <leonidfishkis@gmail.com>
2021-06-20 13:35:11 +0200

    add routing, reinstall apollo client

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,25 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from './Header';
 //import UsersList from './UsersList';
 import RaidsList from './RaidsList';
 import CreateRaid from './CreateRaid';
-//import logo from './../logo.svg';
 import './../styles/App.css';
 
-/* function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={RaidsList} />
+          <Route exact path="/create" component={CreateRaid} />
+        </Switch>
+      </div>
     </div>
   );
-}
- */
-
-
-class App extends Component {
-  render() {
-//    return <UsersList />;
-//  return <RaidsList />;
-  return <CreateRaid />;
-  }
-}
-
+};
 
 
 export default App;
